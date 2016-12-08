4b78b82f82b4a3204bddf4e88be74a815e6bc6b9
yasrul <yasrul@gmail.com>
2016-12-08 13:32:24 +0800

    20161208

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -14,6 +14,7 @@ class SiteController extends Controller
     /**
      * @inheritdoc
      */
+    
     public function behaviors()
     {
         return [
--- views/site/index.php
+++ views/site/index.php
@@ -1,53 +1,31 @@
 <?php
 
 /* @var $this yii\web\View */
+use yii\widgets\Block;
 
-$this->title = 'My Yii Application';
+$this->title = 'E-AdminKada';
 ?>
 <div class="site-index">
 
     <div class="jumbotron">
-        <h1>Congratulations!</h1>
+        <h1>E-AdminKada</h1>
 
-        <p class="lead">You have successfully created your Yii-powered application.</p>
-
-        <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Get started with Yii</a></p>
+        <p class="lead">Sistem Elektronik Administrasi Perkantoran Pemerintah Daerah</p>
+        
     </div>
 
     <div class="body-content">
 
-        <div class="row">
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/doc/">Yii Documentation &raquo;</a></p>
-            </div>
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/forum/">Yii Forum &raquo;</a></p>
-            </div>
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/extensions/">Yii Extensions &raquo;</a></p>
-            </div>
-        </div>
-
+            <?php Block::begin([
+                'id' => 'sidebar'
+            ]); ?>
+            <h1>Menu</h1>
+            <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Contack</li>
+            </ul>
+            <?php Block::end(); ?>
+            
     </div>
 </div>
