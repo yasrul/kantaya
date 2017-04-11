55cffead44667ce0596b3dba7269381ce4c924ab
Yasrul <yasrul@gmail.com>
2017-04-11 09:48:18 +0800

    20170411

--- views/surat-masuk/_form.php
+++ views/surat-masuk/_form.php
@@ -5,6 +5,7 @@ use yii\widgets\ActiveForm;
 use yii\bootstrap\Collapse;
 use kartik\date\DatePicker;
 use kartik\select2\Select2;
+use yii\helpers\ArrayHelper;
 use wbraganca\dynamicform\DynamicFormWidget;
 
 use app\models\KecepatanSampai;
@@ -43,7 +44,7 @@ use app\models\UnitKerja;
     
     <?= $form->field($modelSurat, 'file_arsip')->textInput() ?>
     <?= $form->field($modelSurat, 'id_pengirim')->widget(Select2::className(), [
-        'data' => UnitKerja::listUnit(),
+        'data' => UnitKerja::listUnit(1),
         'options' => ['placeholder' => '[ Pilih Pengirim ]'],
         'pluginOptions' => ['allowClear' => true, 'width'=>'500px']
     ]) ?>
@@ -67,13 +68,14 @@ use app\models\UnitKerja;
                 'widgetContainer' => 'dynamicform_wrapper',  // required: only alphanumeric characters plus "_" [A-Za-z0-9_]
                 'widgetBody' => '.container-items',          // required: css class selector
                 'widgetItem' => '.item',                     // required: css class
-                'limit' => 999,                                // the maximum times, an element can be cloned (default 999)
+                'limit' => 99,                                // the maximum times, an element can be cloned (default 999)
                 'min' => 1,                                  // 0 or 1 (default 1)
                 'insertButton' => '.add-item',               // css class
                 'deleteButton' => '.remove-item',            // css class
                 'model' => $modelTujuan[0],
                 'formId' => 'surat-masuk-form',
                 'formFields' => [
+                    'id_surat',
                     'id_penerima',
                     'penerima_manual',
                     'alamat_manual',
@@ -83,6 +85,7 @@ use app\models\UnitKerja;
             <div class="container-items"><!-- widgetContainer -->
             <?php foreach ($modelTujuan as $i => $tujuan): ?>
                 <div class="item row">
+     
                     <?php
                         // necessary for update action.
                         if (! $tujuan->isNewRecord) {
@@ -90,14 +93,28 @@ use app\models\UnitKerja;
                         }
                     ?>
                     
-                    <?= $form->field($tujuan, "[{$i}]id_penerima")->widget(Select2::className(), [
-                        'data' => UnitKerja::listUnit(),
+                        <?= $form->field($tujuan, "[{$i}]id_penerima")->widget(Select2::className(), [
+                        'data' => UnitKerja::listUnit(Yii::$app->user->identity->unit_id),
                         'options' => ['placeholder' => '[ Pilih Pengirim ]'],
-                        'pluginOptions' => ['allowClear' => true, 'width'=>'500px'],
-                    ]); ?>
-                    <?= $form->field($tujuan, "[{$i}]penerima_manual")->textInput(['maxLength'=>true, 'style'=>'width : 500px']) ?>
-                    <?= $form->field($tujuan, "[{$i}]alamat_manual")->textInput(['maxLength'=>true, 'style'=>'width : 500px']) ?>
-                   
+                        'pluginOptions' => ['allowClear' => true],
+                        ]); ?>
+                    
+                    
+                    
+                        <?php echo Collapse::widget([
+                        'items' => [
+                            [
+                                'label' => 'Pengirim Manual',
+                                'content' => [
+                                    $form->field($tujuan, "[{$i}]penerima_manual")->textInput(['maxLength'=>true]),
+                                    $form->field($tujuan, "[{$i}]alamat_manual")->textInput(['maxLength'=>true])
+                                ]
+                            ]
+                        ]
+                        ]) ?>
+                    
+                          
+                                       
                     <div class="col-sm-2 col-md-1 item-action">
                     	<div class="pull-right">
 	                        <button type="button" class="add-item btn btn-success btn-xs">
@@ -106,6 +123,7 @@ use app\models\UnitKerja;
 	                        	<i class="glyphicon glyphicon-minus"></i></button>
                     	</div>
                     </div>
+                    
                 </div><!-- .row -->
 
             <?php endforeach; ?>
