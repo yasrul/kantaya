993edb511df8343040b0ccbcd4cd75bf9bf34242
yasrul <yasrul@gmail.com>
2017-10-25 16:43:19 +0800

    20171026

--- views/surat-masuk/create.php
+++ views/surat-masuk/create.php
@@ -12,7 +12,7 @@ $this->params['breadcrumbs'][] = ['label'=>'Surat Masuk', 'url'=>['index']];
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="surat-masuk-create">
-    <h1><?= Html::encode($this->title)?></h1>
+    <!--<h1><?= Html::encode($this->title)?></h1>-->
     
     <?= $this->render('_form', [
         'modelSurat'=>$modelSurat,
